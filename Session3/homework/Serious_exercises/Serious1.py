63837464ff91ebe848fb40c651b18738f1dcf37c
taiti308 <tanvn.nevents@gmail.com>
2018-04-16 22:50:28 +0700

    hws3p2

--- Session3/homework/Serious_exercises/Serious1.py
+++ Session3/homework/Serious_exercises/Serious1.py
@@ -2,26 +2,35 @@ crud=['C','R','U','D']
 items=['T-shirt','Sweater']
 
 while True:
-    f= str(input("Welcome to our shop, what do you want (C, R, U, D)?").upper())
+    f= str(input("Welcome to our shop, what do you want (C, R, U, D)?").upper())        #chuyen input ng dung thanh viet hoa het
     print('If you want to exit, please type Ctrl+C and then press Enter, thanks!')
     if f == crud[1]:                            #function Read
-        print(*items, sep='')
+        print(*items, sep=', ')
 
     elif f == crud[0]:                          #function Create
         c = str(input('Enter new item: '))
-        items.append= c
+        items.append(c)
         print(*items, sep=', ')
 
     elif f == crud[2]:                          #function Update
-        u = int(input('Update position: '))
-        n = str(input('New item:'))
-        items[u+1] == n
-        print(*items, sep=', ')
+        while True:
+            u = int(input('Update position: '))-1
+            if u not in range(len(items)):
+                print("invalid index!")
+            else:
+                n = str(input('New item:'))
+                items[u] = n
+                print(*items, sep=', ')
+                break
 
     elif f == crud[3]:                          #function Delete
-        d = int(input('Delete position: '))
-        del items[d+1]
-        print(*items, sep=', ')
-
+        while True:
+            d = int(input('Delete position: '))-1
+            if d not in range(len(items)):
+                print("invalid index!")
+            else:
+                del items[d]
+                print(*items, sep=', ')
+                break
     else:
         print('No such function, please try again!')
--- Session3/homework/Serious_exercises/Serious2.py
+++ Session3/homework/Serious_exercises/Serious2.py
@@ -1,24 +1,25 @@
-size_list = [5, 7, 300, 90, 24, 50, 75]
+list = [5, 7, 300, 90, 24, 50, 75]
 
-print("Hello, my name is Hiep and these are my sheep sizes: ")
-print(size_list, sep=", ")
+print("Hello, my name is Tan and these are my sheep sizes: ")
+print(list, sep=", ")
 
-biggest_sz = max(size_list)
-print("Now my biggest sheep has size", biggest_sz, ", let's shear it!")
-size_list.remove(biggest_sz)
+bigsz = max(list)
+print("Now my biggest sheep has size", bigsz, ", let's shear it!")
+index = list.index(bigsz)
+list[index]=biggest_sz
 print("After shearing, here is my flock: ")
-print(size_list, sep=", ")
-
-for i in range(3):
-    new_list= size_list
-    for size in size_list:
-        size = size + 50
-        new_list.append(size)
-    print("One month has passed, now here is my flock: ")
-    print(size_list, sep=", ")
+print(list, sep=", ")
+While True:
+    print("Please type Ctrl+C and then press Enter if you want to exit!")
+    m=int(input("Enter number of month: "))
+    for i in range(m+1):
+        for j in range(7):
+            list[j] = list[j] + 50
+        print("One month has passed, now here is my flock: ")
+        print(list, sep=", ")
 
 total =0
-for j in size_list:
+for j in range(len(list)):
     total= total + list[j]
 money = total * 2
 print("My flock has size in total: ", total)
