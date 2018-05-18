1d6e6f54d9f90813b4f0d7dedea72ba8224a54ea
tanti308 <tanvn.nevents@gmail.com>
2018-05-18 14:46:49 +0700

    hwweb33

--- web3/homework/optional/query.py
+++ web3/homework/optional/query.py
@@ -8,6 +8,13 @@ class river(Document):
     continent = StringField()
     length = IntField()
 
-all_river = river.objects(continent="Africa")
-for i,river in enumerate(all_river):
+# Chay 1 trong 2 cai
+# Ex8:
+af_river = river.objects(continent="Africa")
+for i,river in enumerate(af_river):
     print("{0}. {1}, {2}: {3}".format(i+1, river['name'], river['continent'], river['length']))
+
+# Ex9:
+# am_river = river.objects(continent="S. America", length__lt=1000)
+# for i,river in enumerate(am_river):
+#     print("{0}. {1}, {2}: {3}".format(i+1, river['name'], river['continent'], river['length']))
